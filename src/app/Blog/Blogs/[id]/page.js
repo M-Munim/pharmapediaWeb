dcd2ef67738a2871e62c326069020643fed4ff1b
munim <muhammadmunimoff330@gmail.com>
2024-06-24 17:03:44 +0500

    blog/blogs

--- src/app/Blog/Blogs/[id]/page.js
+++ src/app/Blog/Blogs/[id]/page.js
@@ -48,8 +48,6 @@ const page = ({ params }) => {
   //     setMessage('An error occurred. Please try again.');
   //   }
   // };
-
-
   return (
     <main>
       {/* <h1>{params.id}</h1> */}
@@ -77,7 +75,7 @@ const page = ({ params }) => {
       </section>
 
       <section className=''>
-        <div className="flex justify-between items-start my-20">
+        <div className="flex justify-between items-start mt-20">
           <div className="h-96 w-1/12">
             <div className="flex flex-col justify-center items-center gap-12" style={{
               color: '#121416'
@@ -159,8 +157,37 @@ const page = ({ params }) => {
               </div>
               <button className="button-filledext my-20">VIEW COMMENTS (0)</button>
             </div>
+
+            <div className=" bg-red-200">
+              <div className="uppercase">
+                <h1 className="">Sign Up for Our Newsletters</h1>
+                <h1 className="">Get notified of the best deals on our WordPress themes.</h1>
+              </div>
+
+              <form className='flex justify-center items-center gap-7'>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  required
+                  className='h-16 w-full rounded-sm bg-transparent p-3 outline-none border-slate-400 border-1'
+                />
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  required
+                  className='h-16 w-full rounded-sm bg-transparent p-3 outline-none border-slate-400 border-1'
+                />
+                <button type="submit" className='button-filledext2'>Subscribe</button>
+              </form>
+
+              <div className="flex mb-8 gap-3 items-center justify-start my-6">
+                <input type="checkbox" id="myCheckbox" className="form-checkbox h-5 w-5 text-blue-600 border-gray-300 rounded outline-none " />
+                <p className="font-medium text-pClr">By checking this box, you confirm that you have read and are agreeing to our terms of use regarding the storage of the data submitted through this form.
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="bg-orange-300 w-3/12 px-6">
+          <div className="w-3/12 px-6">
             <h2 className="font-medium text-2xl mb-6">Follow Us</h2>
 
             <div className="flex items-center justify-center gap-12  mb-6">
@@ -214,9 +241,124 @@ const page = ({ params }) => {
                 </p>
               </div>
             </div>
+
+            <h2 className="font-medium text-2xl mb-4">The Latest</h2>
+
+            <div className="bg-red-300 text-white mb-4">
+              <p className="">10 Habits That Will Change Your Live for the Better If envy and jealousy are impacting your friendships</p>
+
+              <div className="flex justify-center items-center">
+                <p className="text-base">June 21,2022</p>
+                <div className="w-6 border-1 border-white"></div>
+                <CiClock2 />
+                <p className="text-sm">2 minute read</p>
+              </div>
+
+            </div>
+
+            <div className="mb-8">
+              <p className="">10 Habits That Will Change Your Live for the Better If envy and jealousy are impacting your friendships</p>
+
+              <div className="flex justify-center items-center gap-5">
+                <p className="text-base">June 21,2022</p>
+                <div className="w-6 border-1 border-white"></div>
+                <CiClock2 />
+                <p className="text-sm">2 minute read</p>
+              </div>
+            </div>
+
+            <div className="mb-8">
+              <p className="">10 Habits That Will Change Your Live for the Better If envy and jealousy are impacting your friendships</p>
+
+              <div className="flex justify-center items-center gap-5">
+                <p className="text-base">June 21,2022</p>
+                <div className="w-6 border-1 border-white"></div>
+                <CiClock2 />
+                <p className="text-sm">2 minute read</p>
+              </div>
+            </div>
+
+            <div className="mb-8">
+              <p className="">10 Habits That Will Change Your Live for the Better If envy and jealousy are impacting your friendships</p>
+
+              <div className="flex justify-center items-center gap-5">
+                <p className="text-base">June 21,2022</p>
+                <div className="w-6 border-1 border-white"></div>
+                <CiClock2 />
+                <p className="text-sm">2 minute read</p>
+              </div>
+            </div>
           </div>
         </div>
       </section >
+
+      <section className="m-auto overflow-hidden relative">
+        <div className="w-10/12 m-auto flex flex-col md:flex-row items-center justify-center gap-20 relative py-28">
+          <div className="left w-full md:w-1/2 relative z-30">
+            <form action="">
+              <div className="flex justify-between items-center gap-2 md:gap-10 mb-4">
+                <div className="flex flex-col items-start justify-center gap-1 w-6/12">
+                  <label htmlFor="firstName" >First Name</label>
+                  <input type="text" name="firstName" id="firstName" placeholder='First Name' className='h-12 w-full rounded-lg p-3 outline-none border-slate-400 border' required />
+                </div>
+                <div className="flex flex-col items-start justify-center gap-1 w-6/12">
+                  <label htmlFor="lastName" className='text-sm font-medium leading-6'>Last Name</label>
+                  <input type="text" name="lastName" id="lastName" placeholder='Last Name' className='h-12 w-full rounded-lg p-3 outline-none border-slate-400 border' required />
+                </div>
+              </div>
+
+              <div className="flex flex-col mb-4">
+                <label htmlFor="email" className='text-sm font-medium leading-6'>Email</label>
+                <input type="email" name="email" id="email" placeholder='Example@company.com' required className='h-12 w-full rounded-lg p-3 outline-none border-slate-400 border' />
+              </div>
+
+              <div className=" mb-4">
+                <label htmlFor="phone" className='text-sm font-medium leading-6'>Phone number</label>
+                <div className='h-12 w-full rounded-lg outline-none border-slate-400 border phone-input'>
+                  <select id="country-code" name="country-code" className='h-11 rounded-l-lg outline-none'>
+                    <option value="PK">PK +92</option>
+                    <option value="IND">IND +91</option>
+                  </select>
+                  <input type="tel" id="phone" name="phone" placeholder="Phone number" className='h-11 md:w-10/12 px-3 outline-none' />
+                </div>
+              </div>
+
+              <div className="flex flex-col mb-5">
+                <label htmlFor="message" className='text-sm font-medium leading-6'>Message</label>
+                <textarea name="message" id="message" className='h-32 w-full rounded-lg p-3 outline-none border-slate-400 border'></textarea>
+              </div>
+
+              <div className="flex mb-8 gap-3 items-center justify-start">
+                <input type="checkbox" id="myCheckbox" className="form-checkbox h-4 w-4 text-blue-600 border-gray-300 rounded outline-none " />
+                <p className="font-medium text-base">You agree to our friendly <a href="" className='underline'>privacy policy.</a></p>
+              </div>
+
+              <button className="button-filledext">
+                Send Message
+              </button>
+            </form>
+          </div>
+
+          <div className="right w-full md:w-1/2 relative z-30">
+            <div className="flex flex-col gap-4">
+              <h2 className="uppercase font-light text-4xl md:text-5xl text-heading_blue tracking-wider">Get in Touch</h2>
+
+              <p className="text-sm md:text-xl text-t_grey">Lorem ipsum dolor sit amet consectetur. Eu egestas libero viverra vulputate amet nunc lectus non ac. Arcu diam nullam ultrices consectetur. Gravida enim in sagittis mauris aliquam duis.</p>
+
+              <button className="button-filled">
+                Contact us
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-96 h-96 border-2 rounded-full border-grey absolute -bottom-60 -right-36  z-10"></div>
+        <div className="border-2 rounded-full border-grey  absolute -bottom-60 -right-36 z-10" style={{ width: '456px', height: '456px' }}></div>
+        <div className="border-2 rounded-full border-grey opacity-65 absolute -bottom-60 -right-36 z-10" style={{ width: '526px', height: '526px' }}></div>
+        <div className="border-2 rounded-full border-grey opacity-65 absolute -bottom-60 -right-36 z-10" style={{ width: '599px', height: '599px' }}></div>
+
+        <Image src='/Ellipse 45.svg' width={116} height={116} alt="Ellipse Image" className='absolute top-1/3 right-0' />
+      </section>
     </main >
   )
 }
